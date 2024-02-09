871e03d3ef3869843680d4e1858e32f5d3c1a544
Nandini  Sharma <nandini.sharma@impelsys.com>
2024-02-09 12:30:16 +0530

    begining

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Message: "Hello, world!",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
